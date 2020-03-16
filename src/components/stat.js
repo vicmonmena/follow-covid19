66f8940799524e2d65c3dc1571f311e314239c18
vmontanomena <vicente.montanomena@telefonica.com>
2020-03-16 08:50:50 +0100

    FEAT: formateo de números

--- src/components/stat.js
+++ src/components/stat.js
@@ -2,9 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Stat = ({title, value}) => {
+
+  function formatValue(value) {
+    return new Intl.NumberFormat("es-ES").format(value)
+  }
+
   return (
     <div className="stat">
-      <h2>{value}</h2> <h5>{title.toUpperCase()}</h5>
+      <h2>{formatValue(value)}</h2> <h5>{title.toUpperCase()}</h5>
     </div>
   );
 };
