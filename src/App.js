0632525f3b0c777a3e75ba9b73471ac41fd1fa01
vicmonmena <vicmonmena@gmail.com>
2020-03-17 23:47:00 +0100

    FEAT: Incluyendo info sobre la fecha en la última fecha de actualización. Incluyendo selector de países

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import './flat-remix.css';
-import Stat from './components/stat'
+import Stat from './components/stat';
+import Countries from './components/countries';
 
 function App() {
 
-  const [url, setUrl] = useState('https://covid19.mathdro.id/api');
+  const [url, setUrl] = useState('https://covid19.mathdro.id/api')
   const [stats, setStats] = useState({});
   const [loading, setLoading] = useState(false)
   const [confirmed, setConfirmed] = useState(0)
   const [recovered, setRecovered] = useState(0)
   const [deaths, setDeaths] = useState(0)
+  const [lastUpdate, setLastUpdate] = useState(new Date())
   const [error, setError] = useState("")
+  const [selectedCountry, setSelectedCountry] = useState("MUNDIAL")
+  
   useEffect(() => {
-    console.log('Mounting or updating');
+    loadData(url)
+  }, [url]);
+
+  useEffect(() => {
+    setConfirmed(stats.confirmed ?? 0)
+    setRecovered(stats.recovered ?? 0)
+    setDeaths(stats.deaths ?? 0)
+    setLastUpdate(stats.lastUpdate ?? 'no info')
+  }, [stats])
+
+  useEffect(() => {
+    if (selectedCountry === "MUNDIAL") setUrl('https://covid19.mathdro.id/api')
+    else setUrl(`https://covid19.mathdro.id/api/countries/${selectedCountry}`)
+  }, [selectedCountry])
+
+  function loadData(url) {
     async function fetchData() {
       setLoading(true);
-      console.log('Fetching Data');
       const data = await fetch(url)
         .then(res => res.json())
         .catch(err => {
@@ -26,23 +44,46 @@ function App() {
       setLoading(false);
     }
     fetchData();
-  }, [url]);
-  useEffect(() => {
-    console.log('stats: ', stats)
-    setConfirmed(stats.confirmed)
-    setRecovered(stats.recovered)
-    setDeaths(stats.deaths)
-  }, [stats])
+  }
+
+  function formatDate(dateString) {
+    if (dateString) {
+
+      console.log(dateString)
+      const d = new Date(dateString)
+      console.log(d)
+      const ye = new Intl.DateTimeFormat('es-ES', { year: 'numeric' }).format(d)
+      const mo = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(d)
+      const da = new Intl.DateTimeFormat('es-ES', { day: '2-digit' }).format(d)
+      const hr = new Intl.DateTimeFormat('es-ES', { hour: 'numeric' }).format(d)
+      const mn = new Intl.DateTimeFormat('es-ES', { minute: 'numeric' }).format(d)
+      const sc = new Intl.DateTimeFormat('es-ES', { second: 'numeric' }).format(d)
+      return `${da}-${mo}-${ye} a las ${hr}:${mn}:${sc}`
+    } else {
+      return 'no info'
+    }
+  }
+
+  function handleSelectCountry(countrySelected) {
+    setSelectedCountry(countrySelected)
+  }
 
   return (
     <div className="App">
       <h1>Seguimiento sobre el COVID-19 (Nuevo CORONAVIRUS)</h1>
-      <h3>A continuación mostramos las estadísticas sobre los siguientes casos...</h3>
+      <Countries 
+        selectedCountry={selectedCountry}
+        handleSelectCountry={handleSelectCountry} 
+      />
+      <h3>A continuación mostramos las estadísticas sobre los casos...</h3>
+      {loading === false && 
+        <label>Última actualización: {formatDate(lastUpdate)}</label>
+      }
       { loading === true && 
         <p>Cargando ...</p>
       }
       { loading ===false && 
-        <div class="paper stats">
+        <div className="paper stats">
           <Stat 
             title="Confirmados"
             value={confirmed.value}
--- src/components/stat.jsx
+++ src/components/stat.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Stat = ({title, value}) => {
+const Stat = ({title = "no title", value = 0}) => {
 
   function formatValue(value) {
     return new Intl.NumberFormat("es-ES").format(value)
@@ -9,14 +9,14 @@ const Stat = ({title, value}) => {
 
   return (
     <div className="stat">
-      <h2>{formatValue(value)}</h2> <h5>{title.toUpperCase()}</h5>
+      <h2 >{formatValue(value)}</h2> <h5 >{title.toUpperCase()}</h5>
     </div>
   );
 };
 
 Stat.propTypes = {
-  title: PropTypes.string.isRequired,
-  value: PropTypes.number.isRequired
+  title: PropTypes.string,
+  value: PropTypes.number
 };
 
 export default Stat;
