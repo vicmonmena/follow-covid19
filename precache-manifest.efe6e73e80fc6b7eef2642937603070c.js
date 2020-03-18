869ed3a99b1c48d98c62dde233694a814165c6f9
vicmonmena <vicmonena@gmail.com>
2020-03-18 20:08:49 +0100

    Updates

---
+++ precache-manifest.efe6e73e80fc6b7eef2642937603070c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17333ae61a14883f247b10e0f535b1d4",
+    "url": "/follow-covid19/index.html"
+  },
+  {
+    "revision": "bfc4d5a26588e959b239",
+    "url": "/follow-covid19/static/css/main.c7a07148.chunk.css"
+  },
+  {
+    "revision": "5d68977174b89fbbc98d",
+    "url": "/follow-covid19/static/js/2.d22ad668.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/follow-covid19/static/js/2.d22ad668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc4d5a26588e959b239",
+    "url": "/follow-covid19/static/js/main.e1af11d6.chunk.js"
+  },
+  {
+    "revision": "a933b0a17c09613f30cb",
+    "url": "/follow-covid19/static/js/runtime-main.9e1660dd.js"
+  }
+]);
